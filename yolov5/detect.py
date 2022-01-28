69c96d2938fa700b2f76b3324002fd3aedbed611
erickverleye <verleyee@msu.edu>
2022-01-27 19:45:51 -0600

    Fixing path and returning on detect if no classes found

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -6,25 +6,36 @@ import threading
 import numpy as np
 from numpy import random
 
+
+YOLOV5_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
 class Result(ctypes.Structure):
-    _fields_ = [('elem',ctypes.POINTER(ctypes.c_float)),('size', ctypes.c_int)]
+    _fields_ = [('elem', ctypes.POINTER(ctypes.c_float)), ('size', ctypes.c_int)]
+
 
 class COCOConfigTRT:
-    class_name = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
-            "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
-            "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
-            "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
-            "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
-            "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
-            "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
-            "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
-            "hair drier", "toothbrush"]
-    engine_path = 'build/yolov5s.engine'
-    # plugin_library = '/build/libmyplugins.so'
-    execute_file = 'build/yolov5'
+    # TODO: Make a class like this for our custom model
+    class_name = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+                  "traffic light",
+                  "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+                  "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase",
+                  "frisbee",
+                  "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
+                  "surfboard",
+                  "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+                  "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+                  "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard",
+                  "cell phone",
+                  "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                  "teddy bear",
+                  "hair drier", "toothbrush"]
+    engine_path = os.path.join(YOLOV5_PATH, 'build', 'yolov5m.engine')
+    execute_file = os.path.join(YOLOV5_PATH, 'build', 'yolov5')
     score_thr = 0.5
     iou_thr = 0.5
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in class_name]
+
 
 class YOLOV5TRT(object):
     def __init__(self, config):
@@ -38,18 +49,19 @@ class YOLOV5TRT(object):
         self.score_thr = config.score_thr
         self.iou_thr = config.iou_thr
         self.setup()
-        
+
     def setup(self):
         self.engine_strBuffer = ctypes.create_string_buffer(bytes(self.engine_file_path, 'utf-8'))
         self.detector = ctypes.cdll.LoadLibrary(self.excute_file)
         self.detector.detect_img.restype = ctypes.POINTER(Result)
         self.detector.initialize(self.engine_strBuffer)
-        
+
     def detect_img(self, img):
         threading.Thread.__init__(self)
         conf_thr = ctypes.c_float(self.score_thr)
         nms_thr = ctypes.c_float(self.iou_thr)
-        res = self.detector.detect_img(img.shape[0], img.shape[1], img.ctypes.data_as(ctypes.c_char_p), conf_thr, nms_thr)
+        res = self.detector.detect_img(img.shape[0], img.shape[1], img.ctypes.data_as(ctypes.c_char_p), conf_thr,
+                                       nms_thr)
         length = res.contents.size
         assert length % 6 == 0
         pred = np.array([res.contents.elem[i] for i in range(length)])
@@ -57,6 +69,8 @@ class YOLOV5TRT(object):
         result_boxes = pred[:, :4] if len(pred) else np.array([])
         result_scores = pred[:, 4] if len(pred) else np.array([])
         result_classid = pred[:, 5] if len(pred) else np.array([])
+        if not result_boxes:
+            return [], [], []
         result_boxes = self.xywh2xyxy(img.shape[0], img.shape[1], result_boxes)
         result_boxes = result_boxes.astype(np.int)
         result_classid = result_classid.astype(np.int)
@@ -125,8 +139,6 @@ if __name__ == "__main__":
     for i in range(10):
         t1 = time.time()
         boxes, scores, classes = detector.detect_img(img)
-    img = detector.draw_result(img,(boxes, scores, classes))
+    img = detector.draw_result(img, (boxes, scores, classes))
     cv2.imwrite('output.jpg', img)
     detector.destroy()
-
-
